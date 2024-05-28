8b90220d7a9329208f415fd418384d96113961fc
DESKTOP-2K7RL05\fukuz <fukuzawa.h@selecty-hd.co.jp>
2024-05-28 10:24:31 +0900

    Util DbUtil 作成

--- src/main/java/util/DbUtil.java
+++ src/main/java/util/DbUtil.java
@@ -1,5 +1,50 @@
 package util;
 
-public class DbUtil {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
+public class DbUtil {
+	// 定数定義（共通）
+	private static final String DB_HOST = "127.0.0.1";
+	private static final String DB_NAME = "selectydb";
+	private static final String DB_USER = "selectyuser";
+	private static final String DB_PASS = "selecty_2024";
+
+	// 定数定義（MySQL用）
+	private static final String DBMS ="mysql";
+	private static final String DB_DRIVER = "com.mysql.cj.jdbc.Driver";
+	private static final String DB_PORT = "3306";
+	private static final String DB_URL = "jdbc:" + DBMS + "://" + DB_HOST + ":" + DB_PORT + "/" + DB_NAME;
+	//		+ "?serverTimezone=JST";	//JDBCドライバのバージョン8.0.22以前を利用する場合は必要
+
+	// インスタンス化の禁止
+	private DbUtil() {}
+
+	// Connectionを単一のインスタンスとする
+	private static Connection con;
+
+	/**
+	 * DBのコネクションを取得
+	 * @return DBコネクション
+	 * @throws ClassNotFoundException
+	 * @throws SQLException
+	 */
+	public static Connection getConnection(boolean autoCommitFlg) throws ClassNotFoundException, SQLException {
+		if (con == null || con.isClosed()) {
+			Class.forName(DB_DRIVER);
+			con = DriverManager.getConnection(DB_URL, DB_USER, DB_PASS);
+		}
+		con.setAutoCommit(autoCommitFlg);
+		return con;
+	}
+
+	/**
+	 * DBとのコネクションを切断
+	 * @param con DBコネクション
+	 * @throws SQLException
+	 */
+	public static void closeConnection(Connection con) throws SQLException {
+		if (con != null && con.isClosed() != false) { con.close(); }
+	}
 }
--- src/main/java/util/Util.java
+++ src/main/java/util/Util.java
@@ -1,5 +1,21 @@
 package util;
 
 public class Util {
+	private Util() {}
+
+	public static boolean isStringEmpty(String val) {
+		try {
+			if (val == null) {
+				return true;
+			}
+
+			if (val.trim().length() <= 0) {
+				return true;
+			}
+		} catch(Exception e) {
+			return true;
+		}
+		return false;
+	}
 
 }
