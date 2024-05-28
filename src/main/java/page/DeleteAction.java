5e81c935bd885667c8324df308e609508b156b82
DESKTOP-2K7RL05\shima <shimada.k@selecty-hd.co.jp>
2024-05-28 13:46:43 +0900

    DeleteActionを追加

---
+++ src/main/java/page/DeleteAction.java
@@ -0,0 +1,31 @@
+package page;
+
+import javax.servlet.annotation.WebServlet;
+
+import page.base.BaseServlet;
+
+/**
+ * 社員情報削除を行う画面に対応したページクラスです
+ *
+ */
+@WebServlet(name="delete", urlPatterns={"/delete"})
+public class DeleteAction extends BaseServlet {
+	@Override
+	protected String getPageName() {
+		return "deleteConfirm";
+	}
+
+	@Override
+	protected String doAction() throws Exception {
+
+		String[] pageParam = super.getInputParameter(
+				 "empId"		// 0
+		);
+
+		UpdateService service = new UpdateService();
+		service.deleteEmployee(Integer.parseInt(pageParam[0]));
+
+		return "deleteResult";
+	}
+
+}
