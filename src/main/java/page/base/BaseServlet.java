c65adcc8acb5fa0b45c4a5ce34ceea7a73f32ec0
DESKTOP-2K7RL05\torig <torige.a@selecty-hd.co.jp>
2024-05-28 11:17:19 +0900

    pageパッケージ内容を色々変更しました

---
+++ src/main/java/page/base/BaseServlet.java
@@ -0,0 +1,81 @@
+package page.base;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import entity.Employee;
+
+//import com.selecty.example.entity.Department;
+//import com.selecty.example.entity.Employee;
+
+public abstract class BaseServlet extends HttpServlet {
+	//-------------------------------------- プロパティ群
+	/** HttpServletRequest */
+	protected HttpServletRequest request;
+	/** HttpServletResponse */
+	protected HttpServletResponse response;
+	/** HttpSession */
+	protected HttpSession session;
+	/** ErrorMessage */
+	protected String message;
+
+	@Override
+	protected void service(HttpServletRequest arg0, HttpServletResponse arg1) throws ServletException, IOException {
+		arg0.setCharacterEncoding("UTF-8");
+		arg1.setContentType("text/html; charset=UTF-8");
+		arg1.setCharacterEncoding("UTF-8");
+		
+		this.message = null;
+		this.request = arg0;
+		this.response = arg1;
+		this.session = arg0.getSession();
+
+		String nextPage = this.getPageName();
+		try {
+			// ログインチェック
+			if (!"login".equals(this.getPageName())) {
+				if (session != null) {
+					Employee employee = (Employee) session.getAttribute("LOGIN_EMP");
+					
+					
+					if (
+							(employee == null || "".equals(employee.getNmEmployee()))
+							
+						){
+						nextPage = "login";
+						throw new Exception("不正なログイン、またはログイン有効期間が過ぎています");
+						}
+					}
+				}
+			
+
+			// 画面ごとの処理
+			nextPage = this.doAction();
+		} catch (Exception e) {
+			e.printStackTrace();
+			message = e.getMessage();
+		}
+
+		arg0.setAttribute("errMsg", this.message);
+		arg0.getRequestDispatcher(nextPage+".jsp").forward(arg0,arg1);
+	}
+
+	//-------------------------------------- Utilメソッド群
+	protected String[] getInputParameter(String... names) {
+		String[] values = new String[names.length];
+		for (int i = 0; i < names.length; i++) {
+			values[i] = this.request.getParameter(names[i]);
+		}
+		return values;
+	}
+
+	//-------------------------------------- 抽象メソッド群
+	protected abstract String getPageName();
+
+	protected abstract String doAction() throws Exception;
+}
---
+++ src/main/java/service/SearchService.java
@@ -0,0 +1,109 @@
+package service;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import entity.Employee;
+import service.BaseService.BaseService;
+import util.DbUtil;
+
+//import com.selecty.example.dao.DepartmentDao;
+//import com.selecty.example.dao.EmployeeDao;
+//import com.selecty.example.entity.Department;
+//import com.selecty.example.entity.Employee;
+//import com.selecty.example.entity.EmployeeInfo;
+//import com.selecty.example.service.base.BaseService;
+//import com.selecty.example.util.DbUtil;
+
+public class SearchService extends BaseService {
+
+	public SearchService() throws Exception {
+		super(true);
+	}
+
+	/**
+	 * メールアドレス、パスワードでログイン判定を行う
+	 * @param mail
+	 * @param pass
+	 * @return
+	 * @throws Exception
+	 */
+	public Map<String, Object> doLogin(String mail, String pass) throws Exception {
+		Map<String, Object> loginData = new HashMap<>();
+		EmployeeDao dao = new EmployeeDao(this.con);
+		try {
+			loginData.put("EMP", dao.selectLoginInfo(mail, pass));
+			
+		} finally {
+			DbUtil.closeConnection(this.con);
+		}
+		return loginData;
+	}
+
+	
+	
+
+	/**
+	 * 社員検索を行う
+	 * @param form
+	 * @return
+	 */
+	public List<EmployeeInfo> searchEmployeeInfo(Employee emp) throws Exception {
+		EmployeeDao dao = new EmployeeDao(this.con);
+		List<EmployeeInfo> empInfoList = null;
+		try {
+			empInfoList = dao.findByParam(emp);
+			if (empInfoList.size() == 0 || empInfoList == null) {
+			}
+		} finally {
+			DbUtil.closeConnection(this.con);
+		}
+		return empInfoList;
+	}
+
+	/**
+	 * 社員情報の主キー検索を行う
+	 * @param empId
+	 * @return
+	 * @throws Exception
+	 */
+	public Employee searchEmployeeByPkey(Integer empId) throws Exception {
+		EmployeeDao dao = new EmployeeDao(this.con);
+		Employee employee = null;
+		try {
+			employee = dao.findByPramaryKey(empId);
+		} finally {
+			DbUtil.closeConnection(this.con);
+		}
+		return employee;
+	}
+
+	/**
+	 * メールアドレスの重複チェック
+	 * @param id
+	 * @param mail
+	 * @return
+	 * @throws Exception
+	 */
+	public boolean checkDuplicationMail(Integer id, String mail) throws Exception {
+		// メールアドレスで検索を行う
+		EmployeeDao dao = new EmployeeDao(this.con);
+		try {
+			Employee emp = dao.findByMail(mail);
+
+			if (emp != null) {//一致するデータがあったとき
+				if (id == null || !id.equals(emp.getIdEmployee())) {//入力されたIDがデータと一致しているか
+					return true;
+				}
+			}
+		} finally {
+			DbUtil.closeConnection(this.con);
+		}
+		return false;
+	}
+
+}
+
+
+}
